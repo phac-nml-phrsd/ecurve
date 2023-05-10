a1c79683c7ad71dec3dbec8b8b90a9ee0ec2962b
David Champredon <davidchampredon@users.noreply.github.com>
2023-05-10 11:14:32 -0400

    create `print_esc_interval()`

---
+++ R/print_esc_interval.R
@@ -0,0 +1,44 @@
+#' @title Print ESC interval.
+#'
+#' @param x A ESC interval object as returned by the function \code{conc_interval()}.
+#'
+#' @return Print out of the maximum likelihood estimator,
+#' upper and lower bound of the pre-specified statistical confidence level.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' esc_data = data.frame(
+#'    concentrations = c(1,1,10, 10, 100, 500, 500),
+#'    cqs = c(40.2, 39.3, 35.9, 36.4, 32.6, 30.0, 31.1))
+#' model = esc_mle(esc_data)
+#' cqs = new.cqs = c( 34, NaN, 36)
+#' x = conc_interval(cqs = new.cqs, model = model)
+#' print_esc_interval(x)
+#'
+#'
+print_esc_interval <- function(x) {
+
+  if(!inherits(x, 'conc_int')){
+    message('The object is not a ESC `conc_int` object,
+            `print_esc_interval()` cannot print it')
+    return()
+  }
+
+  lvl = x$interval$level
+  lo  = x$interval$lower
+  mle = x$interval$mle
+  hi  = x$interval$upper
+
+
+  cat(paste0(
+    '---- ESC interval ----\n\n',
+    'Confidence level = ', lvl, '\n',
+    'upper bound      = ', hi, '\n',
+    'max. likelihood  = ', mle, '\n',
+    'lower bound      = ', lo, '\n',
+    '----------------------\n'
+  ))
+
+}
